7ae2ad189afb1f0d00d7c53e861771ee71fb89ae
休止 <1158394722@qq.com>
2024-11-22 18:32:44 +0800

    Python常用模块-sys模块

---
+++ src/python_basics/python常用模块/sys模块.py
@@ -0,0 +1,14 @@
+"""
+sys模块
+简介
+sys 是 Python 的内置标准库模块。
+提供了访问与 Python 解释器相关的变量和函数的功能。
+它的主要用途是与系统交互、解释器配置、命令行参数处理、标准输入输出、异常处理等。
+"""
+import sys
+
+print(sys.path)
+print(sys.version)
+print(sys.version_info)
+print(sys.path)
+print(sys.platform)
